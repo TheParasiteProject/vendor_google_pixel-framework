be7e4466d53e1e1848f880bf66fddbb4f799000c
someone5678 <nemui3353@gmail.com>
2023-06-17 12:36:21 +0900

    SystemUIGoogle: Inject custom QS module

--- SystemUIGoogle/src/com/google/android/systemui/dagger/SystemUIGoogleModule.java
+++ SystemUIGoogle/src/com/google/android/systemui/dagger/SystemUIGoogleModule.java
@@ -102,6 +102,8 @@ import com.google.android.systemui.statusbar.KeyguardIndicationControllerGoogle;
 import com.google.android.systemui.statusbar.policy.BatteryControllerImplGoogle;
 import com.google.android.systemui.elmyra.ServiceConfigurationGoogle;
 
+import com.android.systemui.qs.dagger.custom.QSModuleCustom;
+
 import javax.inject.Named;
 
 import dagger.Binds;
@@ -125,7 +127,8 @@ import dagger.Lazy;
         AssistModule.class,
         ElmyraModule.class,
         ColumbusModule.class,
-        StatusBarEventsModule.class
+        StatusBarEventsModule.class,
+        QSModuleCustom.class
 })
 public abstract class SystemUIGoogleModule {
 
