eeb5ebe4d321b5854af0c052be2da3e093cfe29a
someone5678 <someone5678@users.noreply.github.com>
2024-04-14 18:35:59 +0900

    pixel-framework: Update config.mk
    Change-Id: I9961ef951e6a37f1731058a95ce062013877dccb

--- config.mk
+++ config.mk
@@ -1,6 +1,11 @@
 # Inherit from pixel-framework config
 TARGET_INCLUDE_PIXEL_FRAMEWORKS ?= true
 ifeq ($(TARGET_INCLUDE_PIXEL_FRAMEWORKS),true)
-$(call inherit-product, vendor/pixel-framework/common/common-vendor.mk)
-$(call inherit-product, vendor/pixel-framework/products/config.mk)
+$(call inherit-product, vendor/google/pixel-framework/common/common-vendor.mk)
+$(call inherit-product, vendor/google/pixel-framework/products/config.mk)
+
+# Dex preopt
+PRODUCT_DEXPREOPT_SPEED_APPS += \
+    SettingsGoogle \
+    SystemUIGoogle
 endif
