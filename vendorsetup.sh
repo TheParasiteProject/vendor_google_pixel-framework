b7964ea9ed1ecc868ca8cfc2aea8db35969a881e
someone5678 <someone5678@users.noreply.github.com>
2024-04-14 16:19:39 +0900

    pixel-framework: Update customized frameworks setup
    Change-Id: I65994153a9439f2ce4ebbdac61d81f6a22c6b4fb

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,17 +1,24 @@
 #!/bin/bash
 
-cd vendor/pixel-framework
+if [ -z $VENDOR_PIXEL_FRAMEWORK_SETUP_DONE ]; then
+
+APKEDITOR=$(pwd)/prebuilts/tools-parasite/common/bin/APKEditor
+
+cd vendor/google/pixel-framework
 
 if [ -d "products/SystemUIGoogleParasite" ]; then
     cd products/SystemUIGoogleParasite
-    ./../../.bin/APKEditor b -i SystemUIGoogle -f -o SystemUIGoogle.apk
+    $APKEDITOR b -i SystemUIGoogle -f -o SystemUIGoogle.apk
     cd ../../
 fi
 
 if [ -d "products/SettingsGoogleParasite" ]; then
     cd products/SettingsGoogleParasite
-    ./../../.bin/APKEditor b -i SettingsGoogle -f -o SettingsGoogle.apk
+    $APKEDITOR b -i SettingsGoogle -f -o SettingsGoogle.apk
     cd ../../
 fi
 
-cd ../../
+cd ../../../
+
+export VENDOR_PIXEL_FRAMEWORK_SETUP_DONE=true
+fi
